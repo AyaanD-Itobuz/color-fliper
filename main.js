35978b577aa4142bc37f9e612ecd341124c8ded6
AyaanD-Itobuz <ayaan@itobuz.com>
2024-12-27 10:39:37 +0530

    Update

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ container_button = document.getElementById("click");
 function generate() {
     let hex = Math.floor(Math.random() * 1000000)
     const hex_code = '#' + hex;
-    console.log(hex_code) ;
     document.body.style.backgroundColor = hex_code ;
     container_button.innerHTML = hex_code ;
 }
